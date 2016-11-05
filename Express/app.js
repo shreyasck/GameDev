9541170b0991e372d3bfc5ab94a89b03e9d0340c
= <=>
2016-11-05 11:53:28 +0100

    Add coming soon page

--- Express/app.js
+++ Express/app.js
@@ -22,7 +22,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+//app.use('/', index);
+app.use('/',function (req, res) {
+  res.sendFile(path.join(__dirname,'/html/index.html'));
+});
 app.use('/users', users);
 
 // catch 404 and forward to error handler
