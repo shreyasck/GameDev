aed095c267f570b01670c6e0ffeab4692bdff7e8
= <=>
2016-11-06 10:42:20 +0100

    Add PassportJS "local" with local Database for users

--- Express/app.js
+++ Express/app.js
@@ -1,5 +1,45 @@
 var express = require('express');
 var path = require('path');
+
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+var db = require('./db');
+
+// Configure the local strategy for use by Passport.
+//
+// The local strategy require a `verify` function which receives the credentials
+// (`username` and `password`) submitted by the user.  The function must verify
+// that the password is correct and then invoke `cb` with a user object, which
+// will be set at `req.user` in route handlers after authentication.
+passport.use(new Strategy(
+    function(username, password, cb) {
+      db.users.findByUsername(username, function(err, user) {
+        if (err) { return cb(err); }
+        if (!user) { return cb(null, false); }
+        if (user.password != password) { return cb(null, false); }
+        return cb(null, user);
+      });
+    }));
+
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  The
+// typical implementation of this is as simple as supplying the user ID when
+// serializing, and querying the user record by ID from the database when
+// deserializing.
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  db.users.findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -24,6 +64,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 /*app.use(express.static('images'))
 app.use(express.static('javascripts'))
  */
+
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use('/users', users);
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use(express.static('public/stylesheets'));
 app.use(express.static('public/images'));
 app.use(express.static('public/javascripts'));
@@ -35,7 +85,27 @@ app.use('/',function (req, res) {
 });
 
 
-app.use('/users', users);
+
+
+app.get('/login',
+    function(req, res){
+        console.log('Login get');
+
+      res.render('login');
+    });
+
+app.post('/login',
+    passport.authenticate('local', { failureRedirect: '/users' }),
+    function(req, res) {
+        console.log('login post');
+      res.redirect('/');
+    });
+
+app.get('/logout',
+    function(req, res){
+      req.logout();
+      res.redirect('/');
+    });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
