ae5f7c7a1e9874036c0a94bf025c8fbc589de7ca
padmini <padmini.rajanna@student.fh-kiel.de>
2016-11-18 21:16:25 +0100

    Solve Error in Server.js

--- Express/routes/nasir.js
+++ Express/routes/nasir.js
@@ -12,31 +12,6 @@ router.get('/Sayhi', function(req, res, next) {
 
 
 
-router.get('/logout',
-    function(req, res) {
-        //change the logged out status
-        //changeUserFlag(req.user.username, false);
-        User.findOne({username: req.user.username}, function (err, user) {
-            if (!user) {
-                return res.redirect('back');
-            }
-            user.onlineflag = false;
-            user.save(function (err) {
-                //req.logIn(user, function (err) {
-                //     done(err, user);
-                // });
-            });
-            req.logout();
-            res.redirect('/');
-        });
-    });
-function  changeUserFlag(username, status) {
-    //var collection = conn.collection('tbl_user');
-    // console.log("user name:  flag is changed"+ username);
-    // collection.updateOne({"username":username}, {$set:{"onlineflag":status}});
-    User.onlineflag = status;
-}
-
 
 router.get('/startGame',
     require('connect-ensure-login').ensureLoggedIn(),
--- Express/server.js
+++ Express/server.js
@@ -205,6 +205,142 @@ app.get('/signin',
         res.render('login');
         //   res.sendFile('Login.html');
     });
+app.get('/forgotpassword',
+    function(req, res){
+        res.render('forgotpassword');
+        //   res.sendFile('Login.html');
+    });
+
+
+
+/*router.post('/forgotpassword',
+ function(req, res){
+ res.render('login');
+ //   res.sendFile('Login.html');
+ });*/
+
+app.post('/forgotpassword', function(req, res, next) {
+    // console.log("ForgotPassword");
+    async.waterfall([
+        function(done) {
+            crypto.randomBytes(20, function(err, buf) {
+                var token = buf.toString('hex');
+                done(err, token);
+            });
+        },
+        function(token, done) {
+            User.findOne({ email: req.body.email }, function(err, user) {
+                if (!user) {
+                    //console.log("User not found " + req.body.email);
+                    req.flash('error', 'No account with that email address exists.');
+                    return res.redirect('/forgotpassword');
+                }
+                //console.log("token "+ token);
+                user.resetPasswordToken = token;
+                user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+                user.save(function(err) {
+                    done(err, token, user);
+                });
+            });
+        },
+        function(token, user, done) {
+            var Transport = nodemailer.createTransport('SMTP', {
+                service: 'Gmail',
+                auth: {
+                    user: 'sudoku.kiel',
+                    pass: '123ABC456'
+                }
+            });
+
+            var mailOptions = {
+                to: user.email,
+                from: 'passwordreset@demo.com',
+                subject: 'Node.js Password Reset',
+                text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+                'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+                'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+                'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+            };
+            Transport.sendMail(mailOptions, function(err, response) {
+                if(err)
+                    console.log(err);
+                else
+                    console.log("Message sent");
+
+                req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+                done(err, 'done');
+            });
+        }
+    ], function(err) {
+        if (err) return next(err);
+        res.redirect('/forgotpassword');
+    });
+});
+
+app.get('/reset/:token', function(req, res) {
+    User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+        if (!user) {
+            req.flash('error', 'Password reset token is invalid or has expired.');
+            return res.redirect('/forgot');
+        }
+        res.render('reset', {
+            user: req.user
+        });
+    });
+});
+
+app.post('/reset/:token', function(req, res) {
+    async.waterfall([
+        function(done) {
+            //User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+            User.findOne({ email: req.body.email}, function(err, user) {
+                if (!user) {
+                    req.flash('error', 'Password reset token is invalid or has expired.');
+                    return res.redirect('back');
+                }
+                if(req.body.password != req.body.confirmpass)
+                {
+                    done(err, user);
+                }
+
+                user.password = req.body.password;
+                user.resetPasswordToken = undefined;
+                user.resetPasswordExpires = undefined;
+
+                user.save(function(err) {
+                    req.logIn(user, function(err) {
+                        done(err, user);
+                    });
+                });
+            });
+        },
+        function(user, done) {
+            var smtpTransport = nodemailer.createTransport('SMTP', {
+                service: 'Gmail',
+                auth: {
+                    user: 'sudoku.kiel',
+                    pass: '123ABC456'
+                }
+            });
+            var mailOptions = {
+                to: user.email,
+                from: 'passwordreset@demo.com',
+                subject: 'Your password has been changed',
+                text: 'Hello,\n\n' +
+                'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
+            };
+            smtpTransport.sendMail(mailOptions, function(err) {
+                req.flash('success', 'Success! Your password has been changed.');
+                done(err);
+            });
+        }
+    ], function(err) {
+        res.redirect('/');
+    });
+});
+
+
 
 /*app.post('/signup',
     function(req, res){
@@ -226,7 +362,48 @@ function(req, res) {
 
     });*/
 
+app.get('/logout',
+    function(req, res) {
+        //change the logged out status
+        //changeUserFlag(req.user.username, false);
+        User.findOne({username: req.user.username}, function (err, user) {
+            if (!user) {
+                return res.redirect('back');
+            }
+            user.onlineflag = false;
+            user.save(function (err) {
+                //req.logIn(user, function (err) {
+                //     done(err, user);
+                // });
+            });
+            req.logout();
+            res.redirect('/');
+        });
+    });
+function  changeUserFlag(username, status) {
+    //var collection = conn.collection('tbl_user');
+    // console.log("user name:  flag is changed"+ username);
+    // collection.updateOne({"username":username}, {$set:{"onlineflag":status}});
+    User.onlineflag = status;
+}
 
+
+
+app.post('/signup', function(req, res) {
+    var user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        displayName: req.body.fname + " " + req.body.lname
+    });
+
+    user.save(function(err) {
+        req.logIn(user, function(err) {
+            //res.redirect('/login');
+            res.render('home',{ user: req.user ,title:"Sudoku Online Match"});
+        });
+    });
+});
 
 
 
