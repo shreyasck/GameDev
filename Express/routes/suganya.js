d7bfbd59788f1bfa85ab6557cf4d7dd272ef9d52
Suganya Athimuthu <suganya.athimuthu@student.fh-kiel.de>
2016-11-20 23:36:28 +0530

    [FEATURE] Add level selection page
    Signed-off-by: Suganya Athimuthu <suganya.athimuthu@student.fh-kiel.de>

--- Express/routes/suganya.js
+++ Express/routes/suganya.js
@@ -16,9 +16,33 @@ router.get('/signup', function(req, res) {
     });
 });
 
+router.get('/chooseLevel',
+    require('connect-ensure-login').ensureLoggedIn(),
+    function(req, res){
+        res.render('SinglePlayer/SPChooseLevel');//, { user: req.user });
+    });
 
+router.post('/singlePlayer',
+    require('connect-ensure-login').ensureLoggedIn(),
+    function(req, res){
+    if(req.body.easy) {
+        console.log("Ea " + req.body.easy + " " + req.body.intermediate + " " + req.body.hard);
+        // Do Stuffs for handling easy game level
+    }
+    else if(req.body.intermediate){
+        console.log("i " + req.body.easy + " " + req.body.intermediate + " " + req.body.hard);
+        // Do Stuffs for handling intermediate game level
+    }
+        else if(req.body.hard) {
+        console.log("h " + req.body.easy + " " + req.body.intermediate + " " + req.body.hard);
+        // Do Stuffs for handling hard game level
+    }
+            else {
+        console.log("Error");
+    }
 
-
+        res.render('singlePlayer');//, { user: req.user });
+    });
 
 
 module.exports = router;
