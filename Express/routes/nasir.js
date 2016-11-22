f87aa3fcf95e22811bb9368c8b171c16e89d9eaf
nasir.saleh <nasir.saleh@student.fh-kiel.de>
2016-11-22 09:30:14 +0100

    [Features] : (1) Socket IO Send and receive events (2) Save new connected clients to array (3) Callback functions between server and clients (4) Send message from client to another client

--- Express/routes/nasir.js
+++ Express/routes/nasir.js
@@ -1,23 +1,24 @@
+
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
+
+router.get('/mpstart',
+    function(req, res){
+        res.render('../views/multiplayer/MPPlayingPage');
+        //   res.sendFile(__dirname +'/MPPlayingPage.html');
+    });
+
 
 router.get('/Sayhi', function(req, res, next) {
     res.render('error');
 });
-
-
-
-
-
-
-
 router.get('/startGame',
     require('connect-ensure-login').ensureLoggedIn(),
     function(req, res){
         res.render('startGame');//, { user: req.user });
     });
-//app.listen(4000);
+
 
 module.exports = router;
